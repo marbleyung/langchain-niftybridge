12e087dacdaf1b5191c2dc567d79e0bee2fbb0e8
marbleyung <raskolbrd@gmail.com>
2023-05-18 22:57:11 +0300

    bugfix

--- api.py
+++ api.py
@@ -10,17 +10,7 @@ except ModuleNotFoundError:
 app = FastAPI()
 
 
-@app.get('/api/send/')
-async def create_query():
-    return "Ask me something"
-
-
 @app.post('/api/send/')
-<<<<<<< HEAD
 async def create_query(query: Prompt):
-
-=======
-def create_query(query: Prompt):
->>>>>>> 00025123a726e674b3235ab9176285fd3ea7fd58
     answer = get_answer(query=str(query))
     return answer
